83a44079688bd2850ce572e392a36dc743e50425
hungtn <hungtn@amitech.vn>
2020-10-17 23:26:05 +0700

    add admin

---
+++ Areas/web_admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Areas.web_admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        // GET: web_admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/web_admin/web_adminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Blog.Areas.web_admin
+{
+    public class web_adminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "web_admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "web_admin_default",
+                "web_admin/{controller}/{action}/{id}",
+                new { controller= "HomeAdmin", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
