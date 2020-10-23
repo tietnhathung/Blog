e89b25c5a9824289ac341fd2d54af96095a3edc5
hungtn <hungtn@amitech.vn>
2020-10-23 07:28:09 +0700

    them font end

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class DefaultController : Controller
+    {
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+            DataBaseBlog db = new DataBaseBlog();
+            List<Models.Category> list = db.Categories.ToList();
+
+            ViewBag.ListCate = list;
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Blog.Models;
+using Blog.Prepare;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,7 +8,7 @@ using System.Web.Mvc;
 
 namespace Blog.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : DefaultController
     {
 
         public ActionResult Index()
@@ -17,70 +18,45 @@ namespace Blog.Controllers
             return View(blogs);
         }
 
-        //public ActionResult BlogCategory(string searchString, int categoryID = 0)
-        //{
-        //    List<Models.Blog> blogs = PrepareDB.selectListBlogs(20);
+        public ActionResult BlogCategory(string searchString, int categoryID = 0)
+        {
+            return View();
+        }
 
-        //    var categories = from c in mydb.Categories select c;
-        //    ViewBag.categoryID = new SelectList(categories, "ID", "name");
+        public ActionResult Category()
+        {
+            List<Models.Category> cat = PrepareDB.selectListCategorys(20);
+            return View(cat);
+        }
 
-        //    var blogs = from b in mydb.Blogs
-        //                join c in mydb.Categories on b.ID equals c.ID
-        //                select new { b.ID, b.title, b.contents, c.ID, c.name };
+        public ActionResult Detail(string ID)
+        {
+            Models.Blog blog = PrepareDB.selectBlog(ID);
+            return View(blog);
+        }
+        public ActionResult searchByCategory(string id)
+        {
+            DataBaseBlog db = new DataBaseBlog();
 
-        //    if (categoryID != 0)
-        //    {
-        //        blogs = blogs.Where(x => x.CategoryID == categoryID);
-        //    }
+            Category cate = db.Categories.Find(id);
 
-        //    List<Models.Blog> listBlogs = new List<Models.Blog>();
-        //    foreach (var item in blogs)
-        //    {
-        //        Blog temp = new Blog();
-        //        temp.CategoryID = item.CategoryID;
-        //        temp.name = item.name;
-        //        temp.contents = item.contents;
-        //        temp.ID = item.ID;
-        //        temp.title = item.title;
-        //        listBlogs.Add(temp);
-        //    }
+            return View(cate);
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
-        //    if (!String.IsNullOrEmpty(searchString))
-        //    {
-        //        blogs = Blogs.Where(s => s.title.Contains(searchString));
-        //    }
+        public ActionResult searchBlogs(string IbD)
+        {
+            List<Models.Blog> blogs = PrepareDB.selectListBlogs(20);
 
-        //    return View(listBlogs);
-        //}
+            //if (!String.IsNullOrEmpty(ID))
+            //{
+            //}
 
-        //public ActionResult Category()
-        //{
-        //    List<Models.Category> cat = PrepareDB.selectListCategorys(20);
-        //    return View(cat);
-        //}
-
-        //public ActionResult Detail(string ID)
-        //{
-        //    Models.Blog blog = PrepareDB.selectBlog(ID);
-        //    return View(blog);
-        //}
-
-        //public ActionResult Contact()
-        //{
-        //    ViewBag.Message = "Your contact page.";
-        //    return View();
-        //}
-
-        //public ActionResult searchBlogs(string ID)
-        //{
-        //    List<Models.Blog> blogs = PrepareDB.selectListBlogs(20);
-
-        //    if (!String.IsNullOrEmpty(ID))
-        //    {
-        //        blogs = Blogs.Where(s => s.title.Contains(ID));
-        //    }
-
-        //    return View(blogs);
-        //}
+            return View(blogs);
+        }
     }
 }
